4bedb556db49dccfcc0c35d602ca830aa19b930a
Samuel Gonz <4samuelgg@gmail.com>
2024-02-25 11:28:35 +0000

    images

---
+++ src/app/images/page.tsx
@@ -0,0 +1,389 @@
+import Image from "next/image";
+
+const ImagesPage = () => {
+   return (
+      <div>
+         <h1 className="text-center text-2xl text-pink-700 my-4">Imagenes</h1>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>{" "}
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>{" "}
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>{" "}
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>{" "}
+         <div className="my-2">
+            <Image
+               src="/roque.jpg"
+               alt="roque"
+               width={300}
+               height={450}
+               className="aspect-square object-cover"
+            />
+         </div>
+      </div>
+   );
+};
+
+export default ImagesPage;
